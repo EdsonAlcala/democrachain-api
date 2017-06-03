2a0865f3891aac8b89e19fb66eef43bc269fdd71
Edson Alcalá <edson_alcala@msn.com>
2017-06-03 18:32:48 +0100

    database support

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const azure = require('azure');
 const Web3 = require('web3');
 const solc = require('solc');
 const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('DemocrachainDB');
 
 const notificationHubService = azure.createNotificationHubService('democrachainhub', 'Endpoint=sb://democrachain.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=cOJJLfK1QbKGI5HupjEBlecDS4S4vGrIpoUTjxG7GUk=');
 const rpcLocalFromAccount = '0xba6fe142d4e0104c0fbdef7a3fd5ffa122ab712c';
@@ -25,20 +27,84 @@ let votingContractCode = "0x" + votingContractCompiled.contracts[":Voting"].byte
 let votingContractAbi = JSON.parse(votingContractCompiled.contracts[":Voting"].interface);
 console.log("ABI:" + votingContractCompiled.contracts[":Voting"].interface);
 
+//Filling table with information
+db.serialize(function () {
+  db.run("CREATE TABLE IF NOT EXISTS Information (Name TEXT, Value TEXT)");
+  db.run("DELETE FROM Information");
+
+  db.run("INSERT INTO Information VALUES($name, $value)", {
+    $name: "ContractAbi",
+    $value: JSON.stringify(votingContractAbi)
+  });
+
+  db.run("INSERT INTO Information VALUES($name, $value)", {
+    $name: "FromAddress",
+    $value: web3.eth.defaultAccount
+  });
+  db.each("SELECT * FROM Information", function (err, row) {
+    console.log(row.Name + ": " + row.Value);
+  });
+});
+
+db.close();
+
 let votingContract = web3.eth.contract(votingContractAbi);
 let votingContractInstance;
 
-// router.get('/deployToRopsten', function (req, res) {
-//   let httpProviderUrl = "http://23.98.223.9:8545";
-//   let web3 = new Web3(new Web3.providers.HttpProvider(httpProviderUrl));
-//   res.render('index', { title: 'Democrachain API' });
-// });
+// if (process.env.NODE_ENV == 'development') {
+//   deployNewContract();
+// } else {
+//   if (process.env.STAGE == 'newDeployment') {
+//     deployNewContract();
+//   } else {
+//     let deployedContract = votingContract.new(['Yes', 'No'],
+//       { data: votingContractCode, from: web3.eth.defaultAccount, gas: 470000 },
+//       function (err, myContract) {
+//         if (!err) {
+//           if (myContract.address) {
+//             console.log("Address:" + myContract.address);
+//             votingContractInstance = votingContract.at(myContract.address);
+//             var numberVotes = votingContractInstance.GetTotalVotesFor.call('Yes');
+//             console.log("Number of votes: " + numberVotes);
+//             votingContractInstance.VoteForOption.sendTransaction('Yes', { from: web3.eth.defaultAccount }, function (err, result) {
+//               var newNumberOfVotes = votingContractInstance.GetTotalVotesFor.call('Yes');
+//               console.log("New number of votes: " + newNumberOfVotes);
+//             });
+//           }
+//         }
+//       }
+//     );
+//   }
 
-function deployNewContract(){
-  
-}
-if (process.env.NODE_ENV == 'development') {
-  //deployNewContract();
+  // votingContractInstance = votingContract.at('0x09D8eB28b39cF535365372A31fd320AF680C83f1');
+
+  // //getting existing contract
+  // var numberVotes = votingContractInstance.GetTotalVotesFor.call('Yes');
+  // console.log("Number of votes: " + numberVotes);
+
+  // var voteAddedEvent = votingContractInstance.VoteAdded('latest');
+
+  // voteAddedEvent.watch(function (error, result) {
+  //   if (error) {
+  //     console.log(error)
+  //     return;
+  //   }
+  //   console.log(result)
+  //   voteAddedEvent.stopWatching();
+  // });
+
+  // requestAddedEvent.watch(function (error, result) {
+  //   if (error) {
+  //     console.log(error)
+  //     return;
+  //   }
+  //   console.log(result)
+  //   requestAddedEvent.stopWatching();
+  // });
+//}
+
+
+function deployNewContract() {
   let deployedContract = votingContract.new(['Yes', 'No'],
     { data: votingContractCode, from: web3.eth.defaultAccount, gas: 470000 },
     function (err, myContract) {
@@ -56,60 +122,7 @@ if (process.env.NODE_ENV == 'development') {
       }
     }
   );
-
-} else {
-
-  if (process.env.STAGE == 'newDeployment') {
-    //deployNewContract();
-    let deployedContract = votingContract.new(['Yes', 'No'],
-      { data: votingContractCode, from: web3.eth.defaultAccount, gas: 470000 },
-      function (err, myContract) {
-        if (!err) {
-          if (myContract.address) {
-            console.log("Address:" + myContract.address);
-            votingContractInstance = votingContract.at(myContract.address);
-            var numberVotes = votingContractInstance.GetTotalVotesFor.call('Yes');
-            console.log("Number of votes: " + numberVotes);
-            votingContractInstance.VoteForOption.sendTransaction('Yes', { from: web3.eth.defaultAccount }, function (err, result) {
-              var newNumberOfVotes = votingContractInstance.GetTotalVotesFor.call('Yes');
-              console.log("New number of votes: " + newNumberOfVotes);
-            });
-          }
-        }
-      }
-    );
-  } else {
-
-  }
-
-  votingContractInstance = votingContract.at('0x09D8eB28b39cF535365372A31fd320AF680C83f1');
-
-  //getting existing contract
-  var numberVotes = votingContractInstance.GetTotalVotesFor.call('Yes');
-  console.log("Number of votes: " + numberVotes);
-
-  var voteAddedEvent = votingContractInstance.VoteAdded('latest');
-
-  voteAddedEvent.watch(function (error, result) {
-    if (error) {
-      console.log(error)
-      return;
-    }
-    console.log(result)
-    voteAddedEvent.stopWatching();
-  });
-
-  // requestAddedEvent.watch(function (error, result) {
-  //   if (error) {
-  //     console.log(error)
-  //     return;
-  //   }
-  //   console.log(result)
-  //   requestAddedEvent.stopWatching();
-  // });
 }
-
-
 
 
 // watch for an event with {some: 'args'}
