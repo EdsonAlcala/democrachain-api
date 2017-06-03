f4da4a8e2ed99a932590e4b918b2c54976a5162c
Edson Alcalá <edson_alcala@msn.com>
2017-06-03 17:56:20 +0100

    almost there.., i am going to change code to deploy to local and ropsten

--- Contracts/Voting.sol
+++ Contracts/Voting.sol
@@ -2,7 +2,11 @@ pragma solidity ^0.4.2;
 
 contract Voting {
     mapping(bytes32 => uint8) public votesReceived;
+    mapping(address => uint8) public requests;
+
     event VoteAdded(bytes32 option, uint8 votesNumber);
+    event RequestAdded(address sender, uint8 numberOfRequests);
+
     bytes32[] public options;
 
     function Voting(bytes32[] options){
@@ -12,8 +16,11 @@ contract Voting {
     function GetTotalVotesFor(bytes32 option) returns(uint8){
         return votesReceived[option];
     }
-
-    function VoteForOption(bytes32 option){
+    function RequestVoting(){
+        requests[msg.sender] += 1;
+        RequestAdded(msg.sender, requests[msg.sender]);
+    }
+    function VoteForOption(bytes32 option) {
         votesReceived[option] += 1;
         VoteAdded(option, votesReceived[option]);
     }
--- routes/index.js
+++ routes/index.js
@@ -2,45 +2,151 @@ const express = require('express');
 const router = express.Router();
 const azure = require('azure');
 const Web3 = require('web3');
+const solc = require('solc');
 const fs = require('fs');
-let notificationHubService = azure.createNotificationHubService('democrachainhub', 'Endpoint=sb://democrachain.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=cOJJLfK1QbKGI5HupjEBlecDS4S4vGrIpoUTjxG7GUk=');
-let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-let solc = require('solc');
+const notificationHubService = azure.createNotificationHubService('democrachainhub', 'Endpoint=sb://democrachain.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=cOJJLfK1QbKGI5HupjEBlecDS4S4vGrIpoUTjxG7GUk=');
+const httpProviderUrl = process.env.NODE_ENV == 'development' ? "http://localhost:8545" : "http://23.98.223.9:8545";
+const accountCode = process.env.NODE_ENV == 'development' ? '0xba6fe142d4e0104c0fbdef7a3fd5ffa122ab712c' : '0x7688272927dcbc77858c622847ec05fb0a6fadb1';
+let web3 = new Web3(new Web3.providers.HttpProvider(httpProviderUrl));
+
+const rpcLocalFromAccount = '0xba6fe142d4e0104c0fbdef7a3fd5ffa122ab712c';
+const ropstenFromAccount = '0x7688272927dcbc77858c622847ec05fb0a6fadb1';
+
 let code = fs.readFileSync('./Contracts/Voting.sol').toString();
 
-//default account
-web3.eth.defaultAccount = '0x50f1c865895556ed00c1c8c1014e7be029256c67';
-let votingContractCompiled = solc.compile(code, 1);//web3.eth.compile.solidity(code)
-//let votingContractCompiled = web3.eth.compile.solidity(code);
-//let votingContractAbi = votingContractCompiled.info.abiDefinition;
+web3.eth.defaultAccount = accountCode;
+// web3.personal.unlockAccount(accountCode, 'edson123', 15000, function (error, result) {
+//   if (error) {
+//     console.log(error)
+//     return;
+//   }
+//   console.log(result);
+// });
+
+let votingContractCompiled = solc.compile(code, 1);
+
 //using solc
-let votingContractCode = "0x"+votingContractCompiled.contracts[":Voting"].bytecode;
+let votingContractCode = "0x" + votingContractCompiled.contracts[":Voting"].bytecode;
 let votingContractAbi = JSON.parse(votingContractCompiled.contracts[":Voting"].interface);
+console.log("ABI:" + votingContractCompiled.contracts[":Voting"].interface);
 let votingContract = web3.eth.contract(votingContractAbi);
 let votingContractInstance;
-let deployedContract = votingContract.new(['Yes', 'No'],
-  { data: votingContractCode, from: web3.eth.defaultAccount, gas: 470000 },
-  function (err, myContract) {
-    if (!err) {
-      if (myContract.address) {
-        votingContractInstance = votingContract.at(myContract.address);
-        var numberVotes = votingContractInstance.GetTotalVotesFor.call('Yes');
-        console.log("Number of votes: " + numberVotes);
-        votingContractInstance.VoteForOption.sendTransaction('Yes', {from: web3.eth.defaultAccount}, function(err, result){
-          var newNumberOfVotes = votingContractInstance.GetTotalVotesFor.call('Yes');
-          console.log("New number of votes: " + newNumberOfVotes);
-        });
+
+router.get('/deployToRopsten', function (req, res) {
+  let httpProviderUrl = "http://23.98.223.9:8545";
+  let web3 = new Web3(new Web3.providers.HttpProvider(httpProviderUrl));
+  res.render('index', { title: 'Democrachain API' });
+});
+
+if (process.env.NODE_ENV == 'development') {
+
+
+  //deployNewContract();
+  let deployedContract = votingContract.new(['Yes', 'No'],
+    { data: votingContractCode, from: web3.eth.defaultAccount, gas: 470000 },
+    function (err, myContract) {
+      if (!err) {
+        if (myContract.address) {
+          console.log("Address:" + myContract.address);
+          votingContractInstance = votingContract.at(myContract.address);
+          var numberVotes = votingContractInstance.GetTotalVotesFor.call('Yes');
+          console.log("Number of votes: " + numberVotes);
+          votingContractInstance.VoteForOption.sendTransaction('Yes', { from: web3.eth.defaultAccount }, function (err, result) {
+            var newNumberOfVotes = votingContractInstance.GetTotalVotesFor.call('Yes');
+            console.log("New number of votes: " + newNumberOfVotes);
+          });
+        }
       }
     }
+  );
+
+} else {
+
+  if (process.env.STAGE == 'newDeployment') {
+
+  } else {
+
   }
-);
+
+  votingContractInstance = votingContract.at('0x09D8eB28b39cF535365372A31fd320AF680C83f1');
+
+  //getting existing contract
+  var numberVotes = votingContractInstance.GetTotalVotesFor.call('Yes');
+  console.log("Number of votes: " + numberVotes);
+
+  var voteAddedEvent = votingContractInstance.VoteAdded('latest');
+
+  voteAddedEvent.watch(function (error, result) {
+    if (error) {
+      console.log(error)
+      return;
+    }
+    console.log(result)
+    voteAddedEvent.stopWatching();
+  });
+
+  // requestAddedEvent.watch(function (error, result) {
+  //   if (error) {
+  //     console.log(error)
+  //     return;
+  //   }
+  //   console.log(result)
+  //   requestAddedEvent.stopWatching();
+  // });
+}
 
 
-//getting existing contract
+
+
+// watch for an event with {some: 'args'}
+// var events = votingContractInstance.allEvents({ fromBlock: 0, toBlock: 'latest' });
+// events.watch(function (error, result) {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+//   console.log(result);//result.event == 'VoteAdded'
+// });
+// Additionally you can start watching right away, by passing a callback:
+// var filter = web3.eth.filter({ fromBlock: 0, toBlock: 'latest' });
+// filter.watch(function (error, result) {
+//   if (!error)
+//     console.log(result);
+// });
 
 /* GET home page. */
 router.get('/', function (req, res) {
   res.render('index', { title: 'Democrachain API' });
+});
+
+router.get('/request', function (req, res) {
+  return new Promise(function (resolve, reject) {
+    var numberVotes = votingContractInstance.GetTotalVotesFor.call('Yes');
+    console.log("Number of votes: " + numberVotes);
+
+
+    votingContractInstance.RequestVoting.sendTransaction({ from: web3.eth.defaultAccount }, function (error, result) {
+      if (error) {
+        console.log(error);
+        reject(false);
+        return;
+      }
+      var newNumberOfVotes = votingContractInstance.GetTotalVotesFor.call('Yes');
+      console.log("New number of votes: " + newNumberOfVotes);
+    });
+
+    var requestAddedEvent = votingContractInstance.RequestAdded('latest');
+    requestAddedEvent.watch(function (error, result) {
+      if (error) {
+        console.log(error);
+        reject(false);
+        return;
+      }
+      console.log(result)
+      requestAddedEvent.stopWatching();
+      resolve(true);
+    });
+  });
 });
 
 router.get('/notify', function (req, res) {
@@ -77,6 +183,17 @@ router.get('/vote', function (req, res) {
 
   //   //vote
   //   pollContract.vote.sendTransaction(approved, { from: web3.eth.coinbase });
+
+  votingContractInstance.VoteForOption.sendTransaction('Yes', { from: web3.eth.defaultAccount },
+    function (error, result) {
+      if (error) {
+        console.log(error)
+        return;
+      }
+      var newNumberOfVotes = votingContractInstance.GetTotalVotesFor.call('Yes');
+      console.log("New number of votes: " + newNumberOfVotes);
+    });
+
   res.sendStatus(200);
 });
 
